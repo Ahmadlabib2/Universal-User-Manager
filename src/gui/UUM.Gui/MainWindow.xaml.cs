3ba103e06ff93b03344717d0cb42a03033a7b68f
Ahmadlabib2 <ahmadlabib@gmail.com>
2013-05-07 13:12:36 +0200

    Project layout with empty content - references set properly - output paths not all set correctly yet

---
+++ src/gui/UUM.Gui/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿// See LICENCE.txt in the root for conditions of use
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace UUM.Gui
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
