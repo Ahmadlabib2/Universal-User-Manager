b5a8366db16ea029ccfa35567d112265b0becaa6
Jaap de Haan <jaap.dehaan@color-of-code.de>
2013-05-15 15:24:03 +0200

    Added NUnit test projects

---
+++ src/tests/UUM.Engine.Tests/UserTests.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace UUM.Engine.Tests
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	[TestFixture]
+	public class UserTests
+	{
+		
+	}
+}
