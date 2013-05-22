68c4eaaa5f3f9306b4b233696afe7e1a21942e9b
Jaap de Haan <jaap.dehaan@color-of-code.de>
2013-05-22 22:36:00 +0200

    Moved MainWindow to Views/MainWindow and implemented it using a Catel DataWindow

---
+++ src/gui/UUM.Gui/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Catel.MVVM;
+
+namespace UUM.Gui.ViewModels
+{
+    /// <summary>
+    /// MainWindow view model.
+    /// </summary>
+    public class MainWindowViewModel : ViewModelBase
+    {
+        #region Fields
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+        /// </summary>
+        public MainWindowViewModel()
+        {
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the title of the view model.
+        /// </summary>
+        /// <value>The title.</value>
+        public override string Title { get { return "Main Window View model"; } }
+
+        #endregion
+
+        #region Commands
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
---
+++ src/gui/UUM.Gui/Views/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Catel.Windows;
+
+namespace UUM.Gui.Views
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : DataWindow
+	{
+		public MainWindow()
+            : base(DataWindowMode.Custom)
+		{
+			InitializeComponent();
+		}
+	}
+}
