a1a5bbaa8200038b6a18452c9869a979a6d4be66
Ahmadlabib2 <ahmadlabib@gmail.com>
2013-05-24 11:38:27 +0200

    Added user UserpoolView and UserpoolViewModel. -Approach in adding Users. -Approach in removing Users

---
+++ src/framework/UUM.Engine/Models/UserpoolModel.cs
@@ -0,0 +1,41 @@
+﻿// See LICENCE.txt in the root for conditions of use
+using System;
+using System.Runtime.Serialization;
+using Catel.Data;
+
+namespace UUM.Engine.Models
+{
+	/// <summary>
+	///  Userpool abstraction.
+	/// </summary>
+	 [Serializable]
+	public class UserpoolModel : SavableModelBase<UserpoolModel>
+	{
+		public UserpoolModel()
+		{
+		}
+		
+		protected UserpoolModel(SerializationInfo info, StreamingContext context)
+			: base(info, context) { }
+		
+		
+		public string SourceName
+		{
+			get { return GetValue<string>(SourceNameProperty); }
+			set { SetValue(SourceNameProperty, value); }
+		}
+		
+			public static readonly PropertyData SourceNameProperty =
+            RegisterProperty("SourceName", typeof(string));
+			
+		public string User
+		{
+			get { return GetValue<string>(UserProperty); }
+			set { SetValue(UserProperty, value); }
+		}
+		
+			public static readonly PropertyData UserProperty =
+            RegisterProperty("User", typeof(UserModel));	
+
+	}
+}
---
+++ src/gui/UUM.Controls/ViewModels/UserpoolViewModel.cs
@@ -0,0 +1,70 @@
+﻿// See LICENCE.txt in the root for conditions of use
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Xml.Serialization;
+
+using Catel.Data;
+using Catel.MVVM;
+using UUM.Engine.Models;
+
+namespace UUM.Controls.ViewModels
+{
+	#region Constructor
+	/// <summary>
+	/// Description of UserpoolViewModel.
+	/// </summary>
+	public class UserpoolViewModel : ViewModelBase, IXmlSerializable
+	{
+		public UserpoolViewModel(UserpoolModel pool)
+		{
+			Userpool = pool;
+		}
+		#endregion
+	
+		#region Properties
+		public  IList<UserModel> users;
+		
+		public void AddUser(UserModel user)
+			{
+			users.Add(user);
+			}
+			
+		public void RemoveUser(UserModel user)
+			{
+			users.Remove(user);
+			}
+		public void EditUser(UserModel user)
+			{
+			
+			}
+		#endregion
+		
+		[Model]
+        public UserpoolModel Userpool
+        {
+            get { return GetValue<UserpoolModel>(ModelProperty); }
+            private set { SetValue(ModelProperty, value); }
+        }
+
+       
+        public static readonly PropertyData ModelProperty =
+            RegisterProperty("Userpool", typeof(UserpoolModel));
+        
+        
+		public System.Xml.Schema.XmlSchema GetSchema()
+		{
+			throw new NotImplementedException();
+		}
+		
+		public void ReadXml(System.Xml.XmlReader reader)
+		{
+			throw new NotImplementedException();
+		}
+		
+		public void WriteXml(System.Xml.XmlWriter writer)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
