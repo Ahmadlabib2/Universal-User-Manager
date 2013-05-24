7722b1f8d7bc38c365edfadde5190f3f9bf52fda
Jaap de Haan <jaap.dehaan@color-of-code.de>
2013-05-24 11:33:51 +0200

    Added a TraceOutputVindow shown in debug mode as described in http://www.codeproject.com/Articles/151491/Catel-Part-5-of-n-Building-a-WPF-example-applicati

---
+++ src/gui/UUM.Gui/Helpers/TraceOutputWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Catel.Windows;
+
+namespace UUM.Gui.Helpers
+{
+    /// <summary>
+    /// Interaction logic for TraceOutputWindow.xaml
+    /// </summary>
+    public partial class TraceOutputWindow : DataWindow
+    {
+        public TraceOutputWindow()
+            : base(DataWindowMode.Custom, setOwnerAndFocus: false)
+        {
+            InitializeComponent();
+        }
+
+        protected override System.Type GetViewModelType()
+        {
+            return typeof(TraceOutputViewModel);
+        }
+    }
+}
--- src/gui/UUM.Gui/Views/MainWindow.xaml.cs
+++ src/gui/UUM.Gui/Views/MainWindow.xaml.cs
@@ -1,16 +1,34 @@
 ﻿using Catel.Windows;
+using UUM.Gui.Helpers;
 
 namespace UUM.Gui.Views
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : DataWindow
-	{
-		public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DataWindow
+    {
+#if DEBUG
+        private readonly TraceOutputWindow _traceOutputWindow;
+#endif
+
+        public MainWindow()
             : base(DataWindowMode.Custom)
-		{
-			InitializeComponent();
-		}
-	}
+        {
+            InitializeComponent();
+
+#if DEBUG
+            _traceOutputWindow = new TraceOutputWindow();
+            _traceOutputWindow.Show();
+
+            Closed += (sender, e) =>
+                            {
+                                if (_traceOutputWindow != null)
+                                {
+                                    _traceOutputWindow.Close();
+                                }
+                            };
+#endif
+        }
+    }
 }
