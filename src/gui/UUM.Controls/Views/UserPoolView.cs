c48fbb92a1d0add740e83bbd0eceea6cd632711f
Ahmadlabib2 <ahmadlabib@gmail.com>
2013-06-01 22:24:19 +0200

    to commit fixing

---
+++ src/gui/UUM.Controls/Views/UserPoolView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Catel.Windows.Controls;
+
+namespace UUM.Controls.Views
+{
+	/// <summary>
+	/// Interaction logic for UserpoolView..xaml
+	/// </summary>
+	public partial class UserPoolView : UserControl
+	{
+		public UserPoolView()
+		{
+			InitializeComponent();
+		}
+	}
+}
