f6137ab844c2fe9b3d7d88d7e20319d740bfcbdb
Jaap de Haan <jaap.dehaan@color-of-code.de>
2013-06-17 17:28:10 +0200

    Added prrof of concept for setting/getting properties dynamically

--- src/gui/UUM.Controls/ViewModels/RepositorySettingsViewModel.cs
+++ src/gui/UUM.Controls/ViewModels/RepositorySettingsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Catel.Data;
@@ -7,59 +8,89 @@ using UUM.Api.Interfaces;
 
 namespace UUM.Controls.ViewModels
 {
-	/// <summary>
-	///     RepositorySettingsViewModel view model.
-	/// </summary>
-	public class RepositorySettingsViewModel : ViewModelBase
-	{
-		public RepositorySettingsViewModel(IParameters parameters)
-		{
-			Parameters = parameters;
-		}
+    /// <summary>
+    ///     RepositorySettingsViewModel view model.
+    /// </summary>
+    public class RepositorySettingsViewModel : ViewModelBase
+    {
+        public RepositorySettingsViewModel(IParameters parameters)
+        {
+            Parameters = parameters;
+        }
 
-		/// <summary>
-		///     Gets the title of the view model.
-		/// </summary>
-		/// <value>The title.</value>
-		public override string Title
-		{
-			get { return "RepositorySettings"; }
-		}
+        /// <summary>
+        ///     Gets the title of the view model.
+        /// </summary>
+        /// <value>The title.</value>
+        public override string Title
+        {
+            get { return "RepositorySettings"; }
+        }
 
-		#region Properties
+        #region Properties
 
-		#region Property: Parameters
+        #region Property: Parameters
 
-		public static readonly PropertyData ParametersProperty =
-			RegisterProperty("Parameters", typeof (IParameters));
+        public static readonly PropertyData ParametersProperty =
+            RegisterProperty("Parameters", typeof (IParameters));
 
-		[Model]
-		public IParameters Parameters
-		{
-			get { return GetValue<IParameters>(ParametersProperty); }
-			private set { SetValue(ParametersProperty, value); }
-		}
+        [Model]
+        public IParameters Parameters
+        {
+            get { return GetValue<IParameters>(ParametersProperty); }
+            private set { SetValue(ParametersProperty, value); }
+        }
 
-		#endregion
+        #endregion
 
-		#region Property: Properties
-		public IEnumerable<PropertyInfo> Properties
-		{
-			get { return Parameters.GetType().GetProperties().Where(x => x.CanRead && x.CanWrite); }
-		}
-		#endregion
+        #region Property: Properties
 
-		#endregion
+        public IEnumerable<PluginParameter> Properties
+        {
+            get
+            {
+                PropertyInfo[] properties = Parameters.GetType().GetProperties();
+                IEnumerable<PropertyInfo> readWriteProperties = properties.Where(x => x.CanRead && x.CanWrite);
+                return readWriteProperties.Where(x => x.Module.Name != "Catel.Core.dll").Select(x => new PluginParameter(Parameters, x));
+            }
+        }
 
-		#region Commands
+        #endregion
 
-		#endregion
-		
-		protected override void OnPropertyChanged(AdvancedPropertyChangedEventArgs e)
-		{
-			base.OnPropertyChanged(e);
-			if (e.PropertyName == "Parameters")
-				RaisePropertyChanged("Properties");
-		}
-	}
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        protected override void OnPropertyChanged(AdvancedPropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+            if (e.PropertyName == "Parameters")
+                RaisePropertyChanged("Properties");
+        }
+
+        public class PluginParameter
+        {
+            private readonly PropertyInfo _pi;
+            private readonly IParameters _parameters;
+
+            public PluginParameter(IParameters parameters, PropertyInfo pi)
+            {
+                _pi = pi;
+                _parameters = parameters;
+            }
+
+            public string Name { get { return _pi.Name;  } }
+
+            /// <summary>
+            /// Use refection to set/get the value
+            /// </summary>
+            public string Value
+            {
+                get { return Convert.ToString(_pi.GetValue(_parameters, null)); }
+                set { _pi.SetValue(_parameters, value, null); }
+            }
+        }
+    }
 }
