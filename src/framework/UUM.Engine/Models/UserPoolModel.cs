fa8a7f3acc6eccdec2e6237675a9168c9887dd42
Jaap de Haan <jaap.dehaan@color-of-code.de>
2013-11-23 22:30:04 +0100

    Cleanup: Fix indentation and naming convention for parameter names

--- src/framework/UUM.Engine/Models/UserPoolModel.cs
+++ src/framework/UUM.Engine/Models/UserPoolModel.cs
@@ -10,87 +10,88 @@ using UUM.Api.Models;
 
 namespace UUM.Engine.Models
 {
-    /// <summary>
-    ///     UserPool: Pool containing the users from different plugins (i.e different sources).
-    /// </summary>
-    [Serializable]
-    public class UserPoolModel : SavableModelBase<UserPoolModel>
-    	
-    {
-        public UserPoolModel()
-        {
-        }
+	/// <summary>
+	///     UserPool: Pool containing the users from different plugins (i.e different sources).
+	/// </summary>
+	[Serializable]
+	public class UserPoolModel : SavableModelBase<UserPoolModel>
+		
+	{
+		public UserPoolModel()
+		{
+		}
 
-        protected UserPoolModel(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-        }
+		protected UserPoolModel(SerializationInfo info, StreamingContext context)
+			: base(info, context)
+		{
+		}
 
-        #region Property: Users
+		#region Property: Users
 
-        public static readonly PropertyData UserProperty =
-            RegisterProperty("Users", typeof(ObservableCollection<UserModel>), new ObservableCollection<UserModel>());
+		public static readonly PropertyData UserProperty =
+			RegisterProperty("Users", typeof(ObservableCollection<UserModel>), new ObservableCollection<UserModel>());
 
-        public ObservableCollection<UserModel> Users
-        {
-            get { return GetValue<ObservableCollection<UserModel>>(UserProperty); }
-            set { SetValue(UserProperty, value); }
-        }
+		public ObservableCollection<UserModel> Users
+		{
+			get { return GetValue<ObservableCollection<UserModel>>(UserProperty); }
+			set { SetValue(UserProperty, value); }
+		}
 
-        #endregion
+		#endregion
 
-        #region Property: UsersInSources
+		#region Property: UsersInSources
 
-        public static readonly PropertyData UsersInSourcesProperty =
-            RegisterProperty("UsersInSources", typeof(ObservableCollection<UserModel>));
+		public static readonly PropertyData UsersInSourcesProperty =
+			RegisterProperty("UsersInSources", typeof(ObservableCollection<UserModel>));
 
-        public ObservableCollection<UserInSourceBase> UsersInSources
-        {
-            get { return GetValue<ObservableCollection<UserInSourceBase>>(UsersInSourcesProperty); }
-            set { SetValue(UsersInSourcesProperty, value); }
-        }
+		public ObservableCollection<UserInSourceBase> UsersInSources
+		{
+			get { return GetValue<ObservableCollection<UserInSourceBase>>(UsersInSourcesProperty); }
+			set { SetValue(UsersInSourcesProperty, value); }
+		}
 
-        #endregion
+		#endregion
 
-        #region Methods
+		#region Methods
 
-        public void Add(UserModel user)
-        {
-            Users.Add(user);
-        }
+		public void Add(UserModel user)
+		{
+			Users.Add(user);
+		}
 
-        public void Remove(UserModel user)
-        {
-            Users.Remove(user);
-        }
+		public void Remove(UserModel user)
+		{
+			Users.Remove(user);
+		}
 
-        public UserModel FindUserInSourceById(Guid id)
-        {
-            return Users.SingleOrDefault(x => x.Id == id);
-        }
-        
-		 #region Load/Save
-         public static UserPoolModel Load(String _configfile)
+		public UserModel FindUserInSourceById(Guid id)
+		{
+			return Users.SingleOrDefault(x => x.Id == id);
+		}
+		
+		#region Load/Save
+		
+		public static UserPoolModel Load(String filePath)
 		{
 			XmlSerializer serializer = new XmlSerializer(typeof(UserPoolModel));
-			using (var xmlReader = XmlReader.Create(_configfile))
+			using (var xmlReader = XmlReader.Create(filePath))
 			{
 				return serializer.Deserialize(xmlReader) as UserPoolModel;
 			}
 		}
-         
-		public void Save(String _configfile)
+		
+		public new void Save(String filePath)
 		{
-            XmlSerializer serializer = new XmlSerializer(typeof(UserPoolModel));
-            XmlWriterSettings settings = new XmlWriterSettings();
+			XmlSerializer serializer = new XmlSerializer(typeof(UserPoolModel));
+			XmlWriterSettings settings = new XmlWriterSettings();
 			settings.Indent = true;
-			using (var xmlWriter = XmlWriter.Create(_configfile, settings))
+			using (var xmlWriter = XmlWriter.Create(filePath, settings))
 			{
 				serializer.Serialize(xmlWriter, this);
 			}
 		}
 		
-        #endregion
-         #endregion
-    }
+		#endregion
+		#endregion
+	}
 }
