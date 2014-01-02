ea2cf65c1b93e24dd9650816c400f71848c5989d
Ahmadlabib2 <ahmadlabib@gmail.com>
2014-01-02 12:06:38 +0100

    Added ProjectModeltesting class with initial testing of default value of project notEmpty

---
+++ src/tests/UUM.Engine.Tests/ProjectModelTests.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Ahmed-Labib.Wadi
+ * Date: 02.01.2014
+ * Time: 12:04
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+using NSubstitute;
+using NUnit.Framework;
+using UUM.Api.Interfaces;
+using UUM.Api.Models;
+using UUM.Engine.Models;
+
+
+namespace UUM.Engine.Tests
+{
+	[TestFixture]
+	public class ProjectModelTests
+	{
+		[Test]
+		public void PrjectModel_Default_IsNotEmpty()
+		{
+		//Testing project model default value is never empty
+		ProjectModel project1 = new ProjectModel();
+		Assert.IsNotEmpty(project1.Parameters,"It is empty");
+		}
+	}
+}
--- src/tests/UUM.Engine.Tests/UserPoolTests.cs
+++ src/tests/UUM.Engine.Tests/UserPoolTests.cs
@@ -84,6 +84,5 @@ namespace UUM.Engine.Tests
 		}
 		
 		
-		
 	}
 }
